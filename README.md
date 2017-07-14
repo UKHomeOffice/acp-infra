b5ba36c64db89beb9204fc9c08987bfb66721f1f
Rohith <gambol99@gmail.com>
2017-07-14 13:06:46 +0100

    Updates
    - adding descriptions to the outputs
    - updating the readme from autogenerated terraform-docs

--- README.md
+++ README.md
@@ -1,43 +1,26 @@
-# Keto-infra
 
-Will create the minimum VPC infrastructure resources required for a typical k8 cluster built using [keto](https://github.com/UKHomeOffice/keto).
+## Inputs
 
-The resources currently include:
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| availability_zones | A collection of availability zones to build in | list | - | yes |
+| cloudtrail_bucket | A bucket to push the cloudtrail events to | string | `` | no |
+| elb_subnet_offset |  | string | `20` | no |
+| environment | The unique environment being created e.g. dev, production etc. | string | - | yes |
+| nat_subnet_offset |  | string | `30` | no |
+| vpc_cidr | The CIRDR network block | string | `10.111.0.0/16` | no |
 
-- VPC
-- Gateways
-    - Inet (for NAT GW)
-    - Nat (one per az)
-- Subnets (will create one per az)
-    - elbs
-    - instances
+## Outputs
 
-## Variable Defaults
+| Name | Description |
+|------|-------------|
+| default_rt_id | The ID for the default routing table, with IGW attached |
+| elb_cidr | A map containing the availability zone to ELB network CIDR i.e. eu-west-1a -> 10.200.10.0/24 |
+| elb_subnets | A map containing the subnet id's for the ELB subnets |
+| nat_cidr | A map containing the availability zone to NAT network CIDR i.e. eu-west-1a -> 10.200.10.0/24 |
+| nat_gateway_private_ips | A list of the NAT gateway private addresses |
+| nat_gateway_public_ips | A list of the NAT gateway EIP public addresses |
+| nat_subnets | A map containing the subnet id's for the NAT subnets |
+| vpc_id | The ID of the aws vpc which has been created |
+| zone_rt_id | A map containing the route table id per availability zone |
 
-| Variable | Default Value  |
-|----------|----------------|
-| `vpc_cidr`   | `10.111.0.0/16` |
-
-See the file [./variables.tf](./variables.tf) for more details.
-
-## Usage
-
-### From CLI
-
-To create a test VPC and infra:
-```
-$ terraform plan
-$ terraform apply
-```
-
-### As Module
-
-To create a VPC from a module:
-```
-module "keto-infra" {
-  environment = "prod"
-  source      = "https://github.com/UKHomeOffice/keto-infra"
-  vpc_cidr    = "10.200.0.0/16"
-  zones       = ["eu-west-1a", "eu-west-1b"]
-}
-```
--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,47 @@
 ## Module Outputs
 #
 
-output "vpc_id"                  { value = "${aws_vpc.main.id}" }
-output "default_rt_id"           { value = "${aws_route_table.default.id}"}
+output "vpc_id" {
+  description = "The ID of the aws vpc which has been created"
+  value       = "${aws_vpc.main.id}"
+}
 
-output "elb_subnets"             { value = "${zipmap(aws_subnet.elb_subnets.*.availability_zone, aws_subnet.elb_subnets.*.id)}" }
-output "nat_subnets"             { value = "${zipmap(aws_subnet.nat_subnets.*.availability_zone, aws_subnet.nat_subnets.*.id)}" }
-output "zone_rt_id"              { value = "${zipmap(aws_subnet.nat_subnets.*.availability_zone, aws_route_table.az_rts.*.id)}" }
-output "elb_cidr"                { value = "${zipmap(aws_subnet.elb_subnets.*.availability_zone, aws_subnet.elb_subnets.*.cidr_block)}" }
-output "nat_cidr"                { value = "${zipmap(aws_subnet.nat_subnets.*.availability_zone, aws_subnet.nat_subnets.*.cidr_block)}" }
+output "default_rt_id" {
+  description = "The ID for the default routing table, with IGW attached"
+  value       = "${aws_route_table.default.id}"
+}
 
-output "nat_gateway_private_ips" { value = [ "${aws_nat_gateway.nat_gws.*.private_ip}" ] }
-output "nat_gateway_public_ips"  { value = [ "${aws_nat_gateway.nat_gws.*.public_ip}" ] }
+output "elb_subnets" {
+  description = "A map containing the subnet id's for the ELB subnets"
+  value       = "${zipmap(aws_subnet.elb_subnets.*.availability_zone, aws_subnet.elb_subnets.*.id)}"
+}
+
+output "nat_subnets" {
+  description = "A map containing the subnet id's for the NAT subnets"
+  value       = "${zipmap(aws_subnet.nat_subnets.*.availability_zone, aws_subnet.nat_subnets.*.id)}"
+}
+
+output "zone_rt_id" {
+  description = "A map containing the route table id per availability zone"
+  value       = "${zipmap(aws_subnet.nat_subnets.*.availability_zone, aws_route_table.az_rts.*.id)}"
+}
+
+output "elb_cidr" {
+  description = "A map containing the availability zone to ELB network CIDR i.e. eu-west-1a -> 10.200.10.0/24"
+  value       = "${zipmap(aws_subnet.elb_subnets.*.availability_zone, aws_subnet.elb_subnets.*.cidr_block)}"
+}
+
+output "nat_cidr" {
+  description = "A map containing the availability zone to NAT network CIDR i.e. eu-west-1a -> 10.200.10.0/24"
+  value       = "${zipmap(aws_subnet.nat_subnets.*.availability_zone, aws_subnet.nat_subnets.*.cidr_block)}"
+}
+
+output "nat_gateway_private_ips" {
+  description = "A list of the NAT gateway private addresses"
+  value       = [ "${aws_nat_gateway.nat_gws.*.private_ip}" ]
+}
+
+output "nat_gateway_public_ips" {
+  description = "A list of the NAT gateway EIP public addresses"
+  value       = [ "${aws_nat_gateway.nat_gws.*.public_ip}" ]
+}
