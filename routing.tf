250ffab68ecde1cf7b1902fe3c367116c04b14e2
Rohith <gambol99@gmail.com>
2017-07-21 13:53:55 +0100

    Routes
    - need to seperate the route from the declarations in order manage independently

--- routing.tf
+++ routing.tf
@@ -15,15 +15,19 @@ resource "aws_route_table" "default" {
   }
 }
 
+## Add the route for internet gateway
+resource "aws_route" "default_igw" {
+  depends_on                = [ "aws_route_table.default" ]
+
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = "${aws_internet_gateway.main.id}"
+  route_table_id            = "${aws_route_table.default.id}"
+}
+
 ## Availability Zone Routing Tables
 resource "aws_route_table" "az_rts" {
   count  = "${var.nat_gateway ? length(var.zones) : 0}"
   vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = "${element(aws_nat_gateway.nat_gws.*.id, count.index)}"
-  }
 
   tags {
     Env               = "${var.environment}"
@@ -31,3 +35,14 @@ resource "aws_route_table" "az_rts" {
     KubernetesCluster = "${var.environment}"
   }
 }
+
+## Add the route for the zone tables
+resource "aws_route" "zone_routes" {
+  count                     = "${var.nat_gateway ? length(var.zones) : 0}"
+  depends_on                = [ "aws_route_table.az_rts" ]
+
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = "${element(aws_nat_gateway.nat_gws.*.id, count.index)}"
+  route_table_id            = "${element(aws_route_table.az_rts.*.id, count.index)}"
+}
+
