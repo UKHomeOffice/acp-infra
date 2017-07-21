99886e46d1ba55d147d1fa96f2a39c7d36f0c23f
Rohith <gambol99@gmail.com>
2017-07-21 18:19:40 +0100

    Validation
    - adding a drone build file for validation

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 # Provider will inherit from environment variables
 provider "aws" {}
 
@@ -6,13 +5,13 @@ provider "aws" {}
 resource "aws_route53_zone" "zone" {
   count = "${var.dns_zone == "" ? 0 : 1}"
 
-  name  = "${var.dns_zone}"
-  tags  = "${merge(var.tags, map("Name", var.environment), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
+  name = "${var.dns_zone}"
+  tags = "${merge(var.tags, map("Name", var.environment), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
 }
 
 # Create the VPC
 resource "aws_vpc" "main" {
   cidr_block = "${var.vpc_cidr}"
 
-  tags       = "${merge(var.tags, map("Name", var.environment), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
+  tags = "${merge(var.tags, map("Name", var.environment), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
 }
