343162d08166dd8db73b50674d3ea69cfd63071f
Rohith <gambol99@gmail.com>
2017-10-24 11:46:52 +0000

    - adding the full cluster name to the igw

--- gateways.tf
+++ gateways.tf
@@ -4,7 +4,7 @@ resource "aws_internet_gateway" "main" {
 
   tags = "${merge(var.tags,
     map("Name", format("%s.%s", var.environment, var.dns_zone)),
-    map("Env", var.environment, "Role", "internet-gateway"))}"
+    map("Env", format("%s.%s", var.environment, var.dns_zone), "Role", "internet-gateway"))}"
 }
 
 # Create the EIP for the NAT Gateways
